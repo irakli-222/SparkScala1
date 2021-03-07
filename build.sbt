de419f2bdfbb1167f82218f64928dd7e8cc0cb14
Irakli Gelenidze <gelenidze.irakli@gmail.com>
2021-03-08 03:23:03 +0400

    final

--- build.sbt
+++ build.sbt
@@ -7,10 +7,13 @@ scalaVersion := "2.12.12"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.1"
+
 // https://mvnrepository.com/artifact/org.apache.spark/spark-hive
-//libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.1.1" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.1.1" % "provided"
+
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.1"
+
 libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.20.0"
 //libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.1.1"
 //libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
--- src/main/scala/NewPack/NewTT.scala
+++ src/main/scala/NewPack/NewTT.scala
@@ -1,136 +1,101 @@
 package NewPack
 
-import java.io.FileWriter
-
 
 object NewTT {
   import org.apache.log4j.{Level, Logger}
   import org.apache.spark.SparkContext
   import org.apache.spark.sql.functions._
-  import java.sql.Connection
-  import java.sql.DriverManager
-  import java.sql.ResultSet
-  import java.sql.SQLException
-  import java.sql.Statement
-  import java.text.SimpleDateFormat
-  import java.util.Locale
 
 
-
-
-
-    def main(args : Array[String]): Unit ={
+  def newETL(dateFrom: String, dateTo: String): String ={
+//    def main(args : Array[String]): Unit ={
       val sc = new SparkContext("local[*]" , "ScalaProj2")
-
       import org.apache.spark.sql.SparkSession
       val spark = SparkSession
         .builder()
         .appName("ScalaProj2")
+        .master("local")
         .config("spark.some.config.option", "some-value")
         .getOrCreate()
       import spark.implicits._
 
-      val format = new SimpleDateFormat("MM-dd-yyyy")
 
-      val date_from ="2020-07-01"
-      val date_to   ="2020-10-22"
 
-      //      Logger.getRootLogger.setLevel(Level.OFF)
+      val date_from =dateFrom
+      val date_to   =dateTo
 
+
+//    put client data from Database into DataFrame
       val clientData = spark.sqlContext.read.format("jdbc")
         .options(
           Map(
             "url" -> "jdbc:sqlite:src\\other\\bank.db",
-            "dbtable" -> "clients")).load().filter(to_date(col("inp_date"),"MM/dd/yyyy").between(date_from, date_to))
+            "dbtable" -> "clients")).load()//.filter(to_date(col("effect_date"),"MM/dd/yyyy").between(date_from, date_to))
 
-      clientData.show()
 
+
+//    put currency data from Database
       val currencyData = spark.sqlContext.read.format("jdbc")
         .options(
           Map(
             "url" -> "jdbc:sqlite:src\\other\\bank.db",
             "dbtable" -> "currency")).load()
 
-      currencyData.show()
-      //currency  transactions clients
 
 
+//    put transactions' data from Database into DataFrame
       val transactionsData = spark.sqlContext.read.format("jdbc")
         .options(
           Map(
             "url" -> "jdbc:sqlite:src\\other\\bank.db",
             "dbtable" -> "transactions"))
             .load()
-            .select(col ("IBAN").as("IBAN_T"),col("Amount"),col("CurrencyId"),col("inp_date"))
+            .select(col ("IBAN").as("IBAN_T"),col("Amount"),col("CurrencyId"),col("inp_date"),col("ID"))
             .filter(to_date(col("inp_date"),"MM/dd/yyyy")
             .between(date_from, date_to))
 
-      transactionsData.show()
+
+//    calculate counts for IBANs from transactions - will use in next operation
+      val transactionsCountByIban = transactionsData
+        .groupBy($"IBAN_T").agg(count($"ID") as "transaction_count")
 
 
+//    create new dataframe from all joined DFs
+      val allTransactionData = transactionsData.as("t")
+        .join(clientData.as("c"), transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
+        .join(currencyData.as("ccy"), transactionsData("CurrencyID") === currencyData("ID"), "inner")
+        .join(transactionsCountByIban.as("cnt"),transactionsCountByIban("IBAN_T") ===transactionsData("IBAN_T") ,  "inner")
+        .select($"t.inp_date", $"t.IBAN_T", $"t.AMOUNT", $"ccy.CCYFrom", $"ccy.rate", $"cnt.transaction_count", $"c.FirstName",$"c.LastName",$"c.Age")
+        .orderBy($"t.inp_date", $"t.IBAN_T", $"ccy.CCYFrom")
 
+      allTransactionData.show()
+
+//    save DF to Hive
+      allTransactionData.write.mode("overwrite").saveAsTable("FullTransactionData")
+
+
+//    create new DF by aggregating transaction data
       val transAggr = transactionsData.join(clientData, transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
-                      .groupBy($"IBAN",$"FirstName",$"LastName").agg(round(avg($"Amount"),2) as "avg_amount")
+        .groupBy($"IBAN",$"FirstName",$"LastName").agg(round(avg($"Amount"),2) as "avg_amount")
+        .orderBy(desc("avg_amount"))
       transAggr.show()
 
-//      tableData.select(to_date(col("inp_date"),"MM/dd/yyyy").as("inp_date")).show()
-//      tableData.withColumn("FirstName",to_date(col("inp_date"),"MM/dd/yyyy").as("inp_date") )
-//      tableData.show()
+//    write into CSV file
+      transAggr.write.format("csv").save("spark-warehouse\\OutputCSV")
 
+      sc.stop()
+      spark.stop()
 
-    //        val filteredData = tableData
-    //          .select(tableData("FirstName"),  to_date(col("inp_date"),"MM/dd/yyyy").alias("date"))
-    //          .filter($"date".between(date_from, date_to))
-    //
-
-      transAggr.write.format("csv").save("src\\other\\newoutput")
-
-
-
-
-////////////////////////////////////////
-//      val url = "jdbc:sqlite:D:\\ScalaProj2\\src\\other\\bank.db"
-//      val conn = DriverManager.getConnection(url)
-//      val statement = conn.createStatement()
-//      val rs =  statement.executeQuery("select * from clients");
-//      val fw = new FileWriter("D:\\scalaProj2\\src\\other\\newoutput.txt", true) ;
-//      while (rs.next) {
-//        val colval = rs.getString("FirstName")+"\n"
-//        println(colval)
-//        fw.write(colval) ;
-//      }
-//      fw.close()
-//      conn.close()
-////////////////////////////////////////
-
-//      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-//      val myTableNames = metaData.select("clients").map(_.getString(0)).collect()
-//
-//      for (t <- myTableNames) {
-//        println(t.toString)
-//
-//        val tableData = sqlContext.read.format("jdbc")
-//          .options(
-//            Map(
-//              "url" -> "jdbc:sqlite:/D:\\ScalaProj2\\src\\other\\bank.db",
-//              "dbtable" -> t)).load()
-//
-//        tableData.show()
-//      }
-
-
-//      val lines = sc.textFile("D:\\scalaProj2\\src\\other\\test.txt");
-//      val words = lines.flatMap(line => line.split(' '))
-//      val wordsKVRdd = words.map(x => (x,1))
-//      val count = wordsKVRdd.reduceByKey((x,y) => x + y).map(x => (x._2,x._1)).sortByKey(false).map(x => (x._2, x._1)).take(10)
-//      count.foreach(println)
-//        lines.foreach(println)
-//        words.saveAsTextFile("D:\\scalaProj2\\src\\other\\newoutput.txt")
-//
-        sc.stop()
-
-
-
+      return "successfully finished"
     }
+
+
+  def main(args : Array[String]): Unit ={
+
+//  call new function with testing values
+    val testNewETL = newETL("2020-07-01","2020-10-22")
+    println(testNewETL)
+
+  }
 
 }
