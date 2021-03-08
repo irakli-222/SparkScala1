125ae87b0fd90462534a6bbf7c9858cc8a1edfe1
Irakli Gelenidze <gelenidze.irakli@gmail.com>
2021-03-08 15:44:39 +0400

    refactoring1

--- src/main/scala/NewPack/NewTT.scala
+++ src/main/scala/NewPack/NewTT.scala
@@ -5,24 +5,20 @@ object NewTT {
   import org.apache.log4j.{Level, Logger}
   import org.apache.spark.SparkContext
   import org.apache.spark.sql.functions._
+  import org.apache.spark.sql.DataFrame
+
+  val sc = new SparkContext("local[*]" , "ScalaProj2")
+  import org.apache.spark.sql.SparkSession
+  val spark = SparkSession
+    .builder()
+    .appName("ScalaProj2")
+    .master("local")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+  import spark.implicits._
 
 
   def newETL(dateFrom: String, dateTo: String): String ={
-//    def main(args : Array[String]): Unit ={
-      val sc = new SparkContext("local[*]" , "ScalaProj2")
-      import org.apache.spark.sql.SparkSession
-      val spark = SparkSession
-        .builder()
-        .appName("ScalaProj2")
-        .master("local")
-        .config("spark.some.config.option", "some-value")
-        .getOrCreate()
-      import spark.implicits._
-
-
-
-      val date_from =dateFrom
-      val date_to   =dateTo
 
 
 //    put client data from Database into DataFrame
@@ -52,36 +48,23 @@ object NewTT {
             .load()
             .select(col ("IBAN").as("IBAN_T"),col("Amount"),col("CurrencyId"),col("inp_date"),col("ID"))
             .filter(to_date(col("inp_date"),"MM/dd/yyyy")
-            .between(date_from, date_to))
+            .between(dateFrom, dateTo))
 
 
-//    calculate counts for IBANs from transactions - will use in next operation
-      val transactionsCountByIban = transactionsData
-        .groupBy($"IBAN_T").agg(count($"ID") as "transaction_count")
 
-
-//    create new dataframe from all joined DFs
-      val allTransactionData = transactionsData.as("t")
-        .join(clientData.as("c"), transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
-        .join(currencyData.as("ccy"), transactionsData("CurrencyID") === currencyData("ID"), "inner")
-        .join(transactionsCountByIban.as("cnt"),transactionsCountByIban("IBAN_T") ===transactionsData("IBAN_T") ,  "inner")
-        .select($"t.inp_date", $"t.IBAN_T", $"t.AMOUNT", $"ccy.CCYFrom", $"ccy.rate", $"cnt.transaction_count", $"c.FirstName",$"c.LastName",$"c.Age")
-        .orderBy($"t.inp_date", $"t.IBAN_T", $"ccy.CCYFrom")
+//    calling allTransactionData to create new dataframe from all passed DFs
+      val allTransactionData = ProcessAllTransactions(transactionsData: DataFrame, clientData: DataFrame, currencyData: DataFrame )
 
       allTransactionData.show()
 
-//    save DF to Hive
-      allTransactionData.write.mode("overwrite").saveAsTable("FullTransactionData")
 
 
-//    create new DF by aggregating transaction data
-      val transAggr = transactionsData.join(clientData, transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
-        .groupBy($"IBAN",$"FirstName",$"LastName").agg(round(avg($"Amount"),2) as "avg_amount")
-        .orderBy(desc("avg_amount"))
+
+//    calling ProcessTransactionsAgg to create new DF by aggregating transaction data
+      val transAggr = ProcessTransactionsAgg(transactionsData: DataFrame, clientData: DataFrame)
       transAggr.show()
 
-//    write into CSV file
-      transAggr.write.format("csv").save("spark-warehouse\\OutputCSV")
+
 
       sc.stop()
       spark.stop()
@@ -89,7 +72,42 @@ object NewTT {
       return "successfully finished"
     }
 
+  def ProcessAllTransactions(transactionsData: DataFrame, clientData: DataFrame, currencyData: DataFrame): DataFrame = {
+    //    calculate counts for IBANs from transactions - will use in next operation
+    val transactionsCountByIban = transactionsData
+      .groupBy($"IBAN_T").agg(count($"ID") as "transaction_count")
 
+
+    //    create new dataframe from all joined DFs
+    val allTransactionData = transactionsData.as("t")
+      .join(clientData.as("c"), transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
+      .join(currencyData.as("ccy"), transactionsData("CurrencyID") === currencyData("ID"), "inner")
+      .join(transactionsCountByIban.as("cnt"),transactionsCountByIban("IBAN_T") ===transactionsData("IBAN_T") ,  "inner")
+      .select($"t.inp_date", $"t.IBAN_T", $"t.AMOUNT", $"ccy.CCYFrom", $"ccy.rate", $"cnt.transaction_count", $"c.FirstName",$"c.LastName",$"c.Age")
+      .orderBy($"t.inp_date", $"t.IBAN_T", $"ccy.CCYFrom")
+
+    //    save DF to Hive
+    allTransactionData.write.mode("overwrite").saveAsTable("FullTransactionData")
+
+    return allTransactionData
+  }
+
+  def ProcessTransactionsAgg(transactionsData: DataFrame, clientData: DataFrame): DataFrame = {
+    //    create new DF by aggregating transaction data
+    val transAggr = transactionsData.join(clientData, transactionsData("IBAN_T") ===  clientData("IBAN"),  "inner")
+      .groupBy($"IBAN",$"FirstName",$"LastName").agg(round(avg($"Amount"),2) as "avg_amount")
+      .orderBy(desc("avg_amount"))
+    transAggr.show()
+
+    //    write into CSV file
+    transAggr.write.format("csv").save("spark-warehouse\\OutputCSV")
+
+    return transAggr
+  }
+
+
+  
+//Main call
   def main(args : Array[String]): Unit ={
 
 //  calling new function with test values
